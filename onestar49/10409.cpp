42f6b81c42c94772780633fc4a087669505a6538
Issac <410921236@gms.ndhu.edu.tw>
2022-11-14 12:16:26 +0800

    add Uva 10409 Die Game

---
+++ onestar49/10409.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+typedef pair<char, int> Pair;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    #ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+    #endif
+    int n;
+    string s;
+    int north = 2, east = 4, south = 5, west = 3;
+    int top = 1;
+    while (cin >> n) {
+        if (n == 0) break;
+        while (n--) {
+            cin >> s;
+            if (s == "north") {
+                north = top;
+                top = south;
+                south = 7 - north;
+            }
+            else if (s == "east") {
+                east = top;
+                top = west;
+                west = 7 - east;
+            }
+            else if (s == "south") {
+                south = top;
+                top = north;
+                north = 7 - south;
+            }
+            else if (s == "west") {
+                west = top;
+                top = east;
+                east = 7 - west;
+            }
+        }
+        cout << top << '\n';
+    }
+    return 0;
+}

e3d8a051701313937aad426a47e77c8f1a546d41
Issac <410921236@gms.ndhu.edu.tw>
2022-12-13 11:53:19 +0800

    add Uva 10055 Hashmat the Brave Warrior

---
+++ onestar49/10055.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+typedef pair<char, int> Pair;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    #ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+    #endif
+    ll a, b;
+    while (cin >> a >> b) {
+        cout << abs(a - b) << '\n';
+    }
+    return 0;
+}
